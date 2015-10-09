055db446a03125caf03eeee49c19e0938e74692b
Ana maria Martin Garcia <anamary9_bcn@hotmail.com>
2015-10-09 17:04:42 +0200

    aleatorio

---
+++ Practicas/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Dim op1 As Integer
+    Dim op2 As Integer
+    Dim res As New Random
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        op1 = CInt(TextBox1.Text())
+        op2 = CInt(TextBox2.Text())
+
+        TextBox3.Text = res.Next(op1, op2 + 1)
+
+    End Sub
+End Class
